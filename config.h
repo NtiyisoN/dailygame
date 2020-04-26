38bc7be740de02227ffce8a86a34467071bf2afe
wojslaw <wojslawowiczrafal@gmail.com>
2020-04-26 20:42:28 +0200

    Moved configurable values into separate file

---
+++ config.h
@@ -0,0 +1,5 @@
+const char * STRING_FILENAME_SAVE = "0.save";
+const int SECONDS_PER_TICK = 1;
+const int INCOME_TICKS_PER_TIMEUNIT = 0xf;
+const int INCOME_PER_TICK  = 1;
+const int HEALING_COST_PER_HP = 0x4;
