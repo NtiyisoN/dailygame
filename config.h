591432b9ef9088b4aeb288f5ff8442fc20e472e7
wojslaw <wojslawowiczrafal@gmail.com>
2020-04-30 11:32:49 +0200

    Added goal

--- config.h
+++ config.h
@@ -3,6 +3,7 @@ const int SECONDS_PER_TICK = 1;
 const int INCOME_TICKS_PER_TIMEUNIT = 0x1;
 const int INCOME_PER_TICK  = 0x1;
 const int HEALING_COST_PER_HP = 0x1;
+const int HEALING_COST_LEVELS_PER_INCREASE = 0x10;
 const int DEFAULT_MAX_HEALTH = 0x10;
 const int MAX_HEALTH_UPGRADE_PER_LEVEL = 0x4;
 const int MONSTER_HP_BASE = 0x4;
